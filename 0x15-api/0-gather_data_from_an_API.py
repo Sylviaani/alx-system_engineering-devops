9c08dde1df4784a590d78d240d919e7fb4ab444f
Sylviaani <sylviaani26@gmail.com>
2024-01-02 09:42:29 -0800

    gather data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    done = [tasks["title"] for task in todos if task[completed]]
+    done = [task["title"] for task in todos if task("completed") is True]
 
-    print("Employee (employee['name'] is done with tasks({len(done)}/{len(todos)}):")
+    print("Employee {} is done with tasks({len(done)}/{len(todos)}):")
     print("{employee['name']}: {len(done)} {len(todos)}")
     for task in done:
         print("\t{task}")
