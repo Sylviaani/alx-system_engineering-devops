f2dcbcdca3c1152959cf80ae674e4dd904697f4d
Sylviaani <sylviaani26@gmail.com>
2024-01-02 08:54:55 -0800

    gather data

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+if __name__ = "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    done = [tasks["title"] for task in todos if task[completed]]
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(done), len(todos)))
+    [print("\t {}".format(c)) for task in done]
