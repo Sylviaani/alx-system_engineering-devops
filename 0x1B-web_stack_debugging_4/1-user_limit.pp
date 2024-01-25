ce808b451b98bbe9e3eea05ae8ae8471b1358d01
Sylviaani <sylviaani26@gmail.com>
2024-01-25 10:17:32 -0800

    1 user

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,10 @@
+# change OS config to allow holberton
+exec { 'Change soft limit':
+  command  => 'sudo sed -i "s/holberton\ssoft.*/holberton\tsoft\tnofile\t10000/" /etc/security/limits.conf',
+  provider => shell,
+}
+
+exec { 'Change hard limit':
+  command  => 'sudo sed -i "s/holberton\shard.*/holberton\thard\tnofile\t100000/" /etc/security/limits.conf',
+  provider => shell,
+}
